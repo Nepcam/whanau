7f5b80d587c6c5179ed77d4e65cdd016290a17da
nepcam <cameron.nepe@gmail.com>
2018-09-10 17:26:06 +1200

    setting up App

--- client/app.jsx
+++ client/app.jsx
@@ -2,3 +2,12 @@ import React from 'react'
 
 //pull data from the form in whanaungatanga and post
 
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            //building up the state
+        }
+    }
+}
