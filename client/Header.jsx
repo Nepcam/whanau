940755a39d6a7b21eafe08d3e9635a7d1c0d6bee
nepcam <cameron.nepe@gmail.com>
2018-09-18 20:04:09 +1200

    starting on react components

---
+++ client/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div class="header">
+            <h1>NEPE WHANAU</h1>
+            <p><strong>Nau mai haere mai ki te whanau pae tukutuku</strong></p>
+        </div>
+    )
+}
+
+export default Header
